8cb14988dd366ad1aaeb8637e80bcc2641052192
josephsoo <59787025+josephsoo@users.noreply.github.com>
2023-06-29 14:08:20 -0700

    Update folder.py
    make code more clear

--- folder.py
+++ folder.py
@@ -11,8 +11,8 @@ def main():
         print(f"Error: Parent directory '{parent_directory}' does not exist.")
         exit(1)
     # Create a destination directory for all the renamed files to go
-    destination_parent_directory_647 = parent_directory + "_Cav_aligned" 
-    destination_parent_directory_568 = parent_directory + "_PTRF_aligned" 
+    destination_parent_directory_647 = parent_directory + "_aligned_c1" 
+    destination_parent_directory_568 = parent_directory + "_aligned_c2" 
     os.makedirs(destination_parent_directory_647, exist_ok=True)
     os.makedirs(destination_parent_directory_568, exist_ok=True)
 
@@ -28,21 +28,21 @@ def main():
         folder_name = os.path.basename(folder)
     
         # Create a folder for the Cav files to go
-        new_directory_cav = os.path.join(destination_parent_directory_647, folder_name)
-        os.makedirs(new_directory_cav, exist_ok=True)
+        new_directory_c1 = os.path.join(destination_parent_directory_647, folder_name)
+        os.makedirs(new_directory_c1, exist_ok=True)
 
         # Create a folder for the PTRF files to go
-        new_directory_ptrf = os.path.join(destination_parent_directory_568, folder_name)
-        os.makedirs(new_directory_ptrf, exist_ok=True)
+        new_directory_c2 = os.path.join(destination_parent_directory_568, folder_name)
+        os.makedirs(new_directory_c2, exist_ok=True)
 
         # moves aligned_c1 and aligned_c2 to the appropriate folders
         for file in glob.glob(os.path.join(folder, "*")):
             if not os.path.isfile(file):
                 continue
             if ("aligned_c1" in file):
-               shutil.move(file, os.path.join(new_directory_cav, get_name(file)))
+               shutil.move(file, os.path.join(new_directory_c1, get_name(file)))
             if ("aligned_c2" in file):
-                shutil.move(file, os.path.join(new_directory_ptrf, get_name(file)))
+                shutil.move(file, os.path.join(new_directory_c2, get_name(file)))
   
 
 # given a path that ends in .csv, returns the base file name that ends in .txt
